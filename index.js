a135579f9a79e3e67e2478dd722ac1bb1c2a475b
Luis Felipe de Jesus <luisfelipedejesusm@gmail.com>
2021-09-25 21:46:12 -0400

    update. Now it takes into cosideration the depth to make a move

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
+/**
+ * 
+ * Minimax Algorithm implemented in Vanilla Js using a Tic Tac Toe Game :)
+ *                          Made with love <3
+ * 
+ */
+
+// Square element array
 var elements = document.getElementsByClassName("square");
+
+// Player and Bot values in array
 var player = 1, bot = -1;
+
+// Game States
 var gameOver = false;
 
-
+// Adding event listeners to squares
 for (let idx = 0; idx < elements.length; idx++) {
     elements[idx].addEventListener("click", function(){
+
+        // TODO: Make an option to select who player goes first.
+
+        // If game isnt over and no one has played:
         if(!this.innerText && !gameOver){
+            // call play method using X, Y coords in board from user input
             play(+this.getAttribute("el"), +this.parentElement.getAttribute("el"), "X");
+
+            // Make the bot play
             botPlay();
         }
     });
 }
 
-
+/**
+ * Make a play using on of the players. 
+ * Print to screen if anyone won.
+ * 
+ * @param {number} row X Position in board
+ * @param {number} col Y Position in board
+ * @param {number} player Value of wether the bot or player who is playing
+ */
 function play(row, col, player){
+
+    // Select the element clicked
     elements[row+col*3].innerText = player;
+
     let board = getBoard();
     let moves = movesLeft(board);
     let boardValue = evaluate(board)
+
     if(!moves || boardValue != 0){
         gameOver = true;
         document.getElementById('footer').classList.remove("hidden");
@@ -32,6 +62,10 @@ function play(row, col, player){
     }
 }
 
+/**
+ * 
+ * @returns Array representing current board
+ */
 function getBoard(){
     let board = newBoard();
     for(let i = 0; i < 3; i++)
@@ -40,15 +74,30 @@ function getBoard(){
     return board;
 }
 
+/**
+ * 
+ * @returns New blank Board
+ */
 function newBoard(){
     return [[0,0,0],[0,0,0],[0,0,0]];
 }
 
+/**
+ * Clean current Board
+ */
 function cleanBoard(){
     for(let i in elements)
         elements[i].innerText = "";
 }
 
+/**
+ * 
+ * @param {Array} board Board to be evaluated and check if someone won.
+ * @returns Value of board. 
+ *          10 for bot win
+ *          -10 for player win 
+ *          0 for no win at all
+ */
 function evaluate(board){
     for(let i = 0; i < 3; i++){
         // Won Horizontally
@@ -80,12 +129,21 @@ function evaluate(board){
     return 0;
 }
 
+/**
+ * 
+ * @param {Array} board Board to be evaluated
+ * @param {number} depth Current depth in tree
+ * @param {boolean} isMaximizer Whether or not is the maximixer turn 
+ * @returns Score of best move
+ */
 function minimax(board, depth, isMaximizer){
     let score = evaluate(board);
-    score += isMaximizer? depth : -depth;
 
     // Somebody won
-    if(score != 0) return score;
+    if(score != 0) {
+        score -= depth;
+        return score;
+    }
 
     // No more moves left
     if(!movesLeft(board)){
@@ -95,11 +153,15 @@ function minimax(board, depth, isMaximizer){
     if(isMaximizer){
         let bestMove = -Infinity;
 
+        // Traverse all cells in board
         for(var i in board){
             for(var k in board[i]){
                 if(board[i][k] == 0){
+                    // Make the bot move (bot is always the maximizer)
                     board[i][k] = bot;
+                    // Calculate the current move and take the maximum
                     bestMove = Math.max(minimax(board, depth+1, !isMaximizer), bestMove);
+                    // Return board to previous state
                     board[i][k] = 0;
                 }
             }
@@ -108,32 +170,38 @@ function minimax(board, depth, isMaximizer){
     }else{
         let bestMove = Infinity;
 
+        // Traverse all cells in board
         for(let i in board){
             for(let k in board[i]){
                 if(board[i][k] == 0){
+                    // Make the player move (player is always the minimizer)
                     board[i][k] = player;
+                    // Calculate the current move and take the minimum
                     bestMove = Math.min(minimax(board, depth+1, !isMaximizer), bestMove);
+                    // Return board to previous state
                     board[i][k] = 0;
                 }
             }
         }
+        // return best move
         return bestMove;
     }
 }
 
+/**
+ * 
+ * @param {Array} board A Board :) 
+ * @returns The best move for the bot
+ */
 function findBotBestMove(board){
     let bestMove = -Infinity;
     let move = [-1, -1];
-
-    // Hard Coded Stuff :)
-    
 
     for(let i in board){
         for(let j in board[i]){
             if(board[i][j] == 0){
                 board[i][j] = bot;
                 let newMoveValue = minimax(board, 0, false);
-                // console.log([i, j], newMoveValue)
                 if(newMoveValue > bestMove){
                     bestMove = newMoveValue;
                     move = [+i, +j];
@@ -146,6 +214,11 @@ function findBotBestMove(board){
     return move;
 }
     
+/**
+ * 
+ * @param {array} board A board
+ * @returns Number of availables squares in board
+ */
 function movesLeft(board){
     let moves = 0;
     for(let i in board)
@@ -154,11 +227,17 @@ function movesLeft(board){
     return moves;
 }
  
+/**
+ * Make the bot play :)
+ */
 function botPlay(){
     if(!gameOver)
         play(...findBotBestMove(getBoard()), "O");
 }
 
+/**
+ * Reset game
+ */
 function reset(){
     document.getElementById('footer').classList.add("hidden");
     cleanBoard();
